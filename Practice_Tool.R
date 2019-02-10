70606f8f3e7a90031ceabe1c404697079398560c
heathermkrause <heather@idatassist.com>
2019-02-10 11:34:06 -0500

    finalizing comments

--- Practice_Tool.R
+++ Practice_Tool.R
@@ -13,8 +13,12 @@ bal.tab(covs0, treat = lalonde$treat, weights = lalonde$att.weights,
 
 #Import the dataframe
 
-#The user puts the name of the variable into the form associated with each column
+#The user puts the name of the key identity variables into the form associated with each column
 
 #The user is asked whether there is a treatment/control variable. If so the user put the name of that variable into the form and tells which column it is located in
 
-#The algorithm assesses the balance
+#The algorithm assesses the balance of representation in each of the identity variables
+
+#The algorithm assesses the balance of the treatment/control in each of the identity variables
+
+#The algorithm assess whether there is much more missing data associated outcome variable and one of the key identity variables
