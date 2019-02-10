aa0ab32b5e6d64212fb20d5248b22b08fc4fd6f6
heathermkrause <heather@idatassist.com>
2019-02-10 11:32:03 -0500

    comments

--- Practice_Tool.R
+++ Practice_Tool.R
@@ -10,3 +10,11 @@ lalonde$att.weights <- with(lalonde, treat + (1-treat)*p.score/(1-p.score))
 
 bal.tab(covs0, treat = lalonde$treat, weights = lalonde$att.weights,
         method = "weighting")
+
+#Import the dataframe
+
+#The user puts the name of the variable into the form associated with each column
+
+#The user is asked whether there is a treatment/control variable. If so the user put the name of that variable into the form and tells which column it is located in
+
+#The algorithm assesses the balance
