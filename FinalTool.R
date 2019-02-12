588aa047a05b619ada430fd560360173e187fd00
Heather Krause <heather@idatassist.com>
2019-02-12 13:10:07 -0500

    annotate

--- FinalTool.R
+++ FinalTool.R
@@ -1,2 +1,8 @@
 #Final tool WAC - Quantitative Analysis
+#Note: currently this only works with categorical identity variables - like gender, sex, ethnicity, income level, geography. It doesn't yet work with continuous variables like income on a continuous scale or height on a continuous scale
 
+#Step 1: UX: User is asked to upload their data file as either csv or Excel
+#Step 2: UX: User is asked to define their key identity variables within the dataset. Maximum of 10
+#Step 3: These variables must be in the first 10 columns of the csv of Excel dataset
+#Step 3a: UX: "What is the name of the key identity variable in the first column?  (For example, gender or ethnicity)
+#Step 3b: UX: "What are the codes for the different 
