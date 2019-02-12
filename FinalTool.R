df61801615f1c36040c38727b00dfdf0e7b2f616
Heather Krause <heather@idatassist.com>
2019-02-12 13:36:15 -0500

    correctly numbering the steps

--- FinalTool.R
+++ FinalTool.R
@@ -6,17 +6,22 @@
 #Step 2: UX: User is asked to define their key identity variables within the dataset. Maximum of 10
 #Step 3: These variables must be in the first 10 columns of the csv of Excel dataset
 #Step 3a: UX: "What is the name of the key identity variable in the first column?  (For example, gender or ethnicity)
-#step 3b: The webtool needs to ingest this list of up to ten names as a vector saved as "keyvarXnames" where the X is replaced by a number between 1 and 10 - once for each key identity variables
-#Step 3b: UX: "What are the codes for the different levels of your key identity variable (for example, for key identity variable #1 is gender where 1 = male and 2 - female)
-#Step 3b: Webtool needs to ingest this vector as "keyvarXcodes" where the X is replaced by a number between 1 and 10 - once for each key identity variables
-#Step 3c: UX: The UX then needs to show each of these levels and ask the user to enter the proportion of this subgroup in the population (or the target proption)
-#Step 4: The webtool then needs to ingest these target proportions as a vector saved as "keyvar1pr"
-#Step 5: Repeat steps 3 and 4 for each of the key identity variables (up to 10)
+#step 3b: The webtool needs to ingest this list of up to ten names as a vector saved as "keyvarXname" where the X is replaced by a number between 1 and 10 - once for each key identity variables
+#Step 3c: UX: "What are the codes for the different levels of your key identity variable (for example, for key identity variable #1 is gender where 1 = male and 2 - female)
+#Step 3d: Webtool needs to ingest this vector as "keyvarXcodes" where the X is replaced by a number between 1 and 10 - once for each key identity variables
+#Step 3e: UX: The UX then needs to show each of these levels and ask the user to enter the proportion of this subgroup in the population (or the target proption)
+#Step 4: The webtool then needs to ingest these target proportions as a vector saved as "keyvarXpr" where the X is replaced by a number between 1 and 10 - once for each key identity variables
+#Step 5: Repeat steps 3 and 4 for each of the key identity variables (up to 10) importing and saving 
 
 
 dd <- #Insert path from webtool here to get dataset
 
+#example dataset
 dd<- read.csv("/Users/heatherkrause/Dropbox/R/WACQuantitativeAssessmentTool/Example data.csv",na.strings=c(""," ","98","99","NA"))
+
+keyvar1name <- "gender"
+keyvar1codes <- c(1,2)
+keyvar1pr <- c(.5,.5)
   
 KeyVar1 <- dd[1]
 countKeyVar1.1 <- length(which(KeyVar1 == 1)) 
